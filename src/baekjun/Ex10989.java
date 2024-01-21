76e24b3fc6d1a8ecdb7ec73e84d742cb95cbc86b
Chenowoo <cjsgusdn14@naver.com>
2024-01-21 10:41:31 +0900

    정렬

---
+++ src/baekjun/Ex10989.java
@@ -0,0 +1,31 @@
+package baekjun;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex10989 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[10001];
+		
+		for (int i = 0; i < n; i++) {
+			arr[Integer.parseInt(br.readLine())]++;
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] > 0) {
+				for (int j = 0; j < arr[i]; j++) {
+					sb.append(i).append("\n");
+				}
+			}
+		}
+		
+		System.out.println(sb);
+	}
+
+}
