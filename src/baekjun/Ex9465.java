e86104f205528da99ff97a8daf2048baf7b0696f
Chenowoo <cjsgusdn14@naver.com>
2024-02-04 11:26:19 +0900

    dp

--- src/baekjun/Ex9465.java
+++ src/baekjun/Ex9465.java
@@ -40,8 +40,6 @@ public class Ex9465 {
 					stickers[j][k] = Integer.parseInt(st.nextToken());
 				}
 			}
-//			dp[0][n] = max(dp[1][n-2], dp[0][n-2]+d[1][n-1])
-//			dp[1][n] = max(dp[0][n-2], dp[0][n-1]+d[1][n-2])
 			
 			dp[0][1] = stickers[0][1];
 			dp[1][1] = stickers[1][1];
@@ -52,13 +50,6 @@ public class Ex9465 {
 				
 				dp[0][j] = Math.max(maxIndex0 + stickers[0][j], dp[0][j]);
 				dp[1][j] = Math.max(maxIndex1 + stickers[1][j], dp[1][j]);
-			}
-			
-			for (int j = 0; j < 2; j++) {
-				for (int k = 1; k < n+1; k++) {
-					System.out.print(dp[j][k] + " ");
-				}
-				System.out.println();
 			}
 			
 			int answer = Math.max(dp[0][n], dp[1][n]);
