fb90a266a2b0b33d9c34936a8d22e5b6a8d96908
Chenowoo <cjsgusdn14@naver.com>
2024-02-17 17:38:45 +0900

    브루트 포스

--- src/baekjun/Ex1107.java
+++ src/baekjun/Ex1107.java
@@ -7,130 +7,41 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
-/*
- * 아이디어
- * n이 주어지면 n부터 -/+하면서
- * 고장난 버튼이 없는 수가 나올 때까지 카운트세기
- * 현재 채녈은 100
- * 100에서 +/-비교
- * n부터 +/-비교
- * 
- * 시간복잡도
- * n
- * 
- * 자료구조
- * 고장난 버튼을 저장할 List
- */
-
 public class Ex1107 {
-	private static int m;
-	private static List<String> breakButtons;
 
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int n = Integer.parseInt(br.readLine());
-		m = Integer.parseInt(br.readLine());
-		breakButtons = new ArrayList<>();
+		int m = Integer.parseInt(br.readLine());
+		boolean[] brokenButtons = new boolean[10];
 		
-		if (m == 0) {
-			System.out.println((int)Math.log10(n)+1);
-		} else {
+		if (m != 0) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for (int i = 0; i < m; i++) {
-				int breakButton = Integer.parseInt(st.nextToken()); 
-				breakButtons.add(String.valueOf(breakButton));
+				brokenButtons[Integer.parseInt(st.nextToken())] = true;
 			}
-			int answer = Math.min(plusButton(n), minusButton(n));
-			answer = Math.min(answer, checkPlusHundred(n));
-			answer = Math.min(answer, checkMinusHundred(n));
-			System.out.println(answer);
 		}
 		
-	}
-
-	private static int plusButton(int n) {
-		int len = (int)Math.log10(n)+1;
-		int count = 0;
-		while (true) {
-			String buttonNumber = String.valueOf(n);
+		int result = Math.abs(n - 100);
+		for (int i = 0; i <= 999999; i++) {
+			String str = String.valueOf(i);
+			int len = str.length();
+			
 			boolean check = false;
-			
-			for (int i = 0; i < m; i++) {
-				if (buttonNumber.contains(breakButtons.get(i))) {
-					count++;
+			for (int j = 0; j < len; j++) {
+				if (brokenButtons[str.charAt(j) - '0']) {
 					check = true;
-					n++;
 					break;
 				}
-			}
-			if (!check || n == 500_000) break;
-		}
-		return (count + len);
-	}
-	
-	private static int minusButton(int n) {
-		int len = (int)Math.log10(n)+1;
-		int count = 0;
-		while (n >= 0) {
-			String buttonNumber = String.valueOf(n);
-			boolean check = false;
-			
-			for (int i = 0; i < m; i++) {
-				if (buttonNumber.contains(breakButtons.get(i))) {
-					count++;
-					check = true;
-					n--;
-					break;
-				}
-			}
-			if (check && n == -1) {
-				return Integer.MAX_VALUE;
-			}
-			if (!check) break;
-		}
-		return (count + len);
-	}
-	
-	private static int checkPlusHundred(int n) {
-		int count = 0;
-		int start = 100;
-		while (true) {
-			if (start == n) break;
-			if (start == 500001) {
-				return Integer.MAX_VALUE;
 			}
 			
-			start++;
-			count++;
-			String buttonNumber = String.valueOf(start);
-			for (int i = 0; i < m; i++) {
-				if (buttonNumber.contains(breakButtons.get(i))) {
-					break;
-				}
+			if (!check) {
+				int min = Math.abs(n - i) + len;
+				result = Math.min(min, result);
 			}
 		}
-		return count;
-	}
-	
-	private static int checkMinusHundred(int n) {
-		int count = 0;
-		int start = 100;
-		while (true) {
-			if (start == n) break;
-			if (start == -1) {
-				return Integer.MAX_VALUE;
-			}
-			
-			start--;
-			count++;
-			String buttonNumber = String.valueOf(start);
-			for (int i = 0; i < m; i++) {
-				if (buttonNumber.contains(breakButtons.get(i))) {
-					break;
-				}
-			}
-		}
-		return count;
+		
+		System.out.println(result);
 	}
 }
