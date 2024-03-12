3087b72901831c8d1958379a160ca989f4ba967a
Chenowoo <cjsgusdn14@naver.com>
2024-03-12 18:55:29 +0900

    스택/큐

--- src/programmers/Ex42584.java
+++ src/programmers/Ex42584.java
@@ -12,22 +12,6 @@ import java.util.PriorityQueue;
  */
 
 public class Ex42584 {
-	
-	public static class Price implements Comparable<Price>{
-		int value;
-		int index;
-		
-		public Price(int value, int index) {
-			this.value = value;
-			this.index = index;
-		}
-		
-		public int compareTo(Price other) {
-			return this.value - other.value;
-		}
-	}
-
-
 	public static void main(String[] args) {
 		int[] prices = {1,2,3,2,3};
 		
@@ -36,14 +20,21 @@ public class Ex42584 {
 		for (int val : arr) {
 			System.out.print(val + " ");
 		}
+		
 	}
 	
 	public static int[] solution(int[] prices) {
-		PriorityQueue<Price> pq = new PriorityQueue<>();
+		int[] answer = new int[prices.length];
 		
 		for (int i = 0; i < prices.length; i++) {
-			pq.offer(new Price(prices[i], i));
+			
+			for (int j = i+1; j < prices.length; j++) {
+				answer[i]++;
+                if (prices[i] > prices[j]) break;
+            }
 		}
+		
+		return answer;
 	}
 
 }
