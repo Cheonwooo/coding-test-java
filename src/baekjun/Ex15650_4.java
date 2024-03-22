f79e55f9567c75f4d32dfe2353689e4a26b1b1ca
Chenowoo <cjsgusdn14@naver.com>
2024-03-22 18:18:20 +0900

    조합

---
+++ src/baekjun/Ex15650_4.java
@@ -0,0 +1,46 @@
+package baekjun;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Ex15650_4 {
+	private static int n, m;
+	private static int[] arr;
+	private static boolean[] visited;
+	private static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		arr = new int[m];
+		visited = new boolean[n];
+		
+		comb(0, 0);
+		System.out.println(sb);
+	}
+
+	public static void comb(int start, int depth) {
+		if (depth == m) {
+			for (int val : arr) {
+				sb.append(val).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for (int i = start; i < n; i++) {
+			if (!visited[i]) {
+				visited[i] = true;
+				arr[depth] = i+1;
+				comb(i+1, depth+1);
+				visited[i] = false;
+			}
+		}
+	}
+}
---
+++ src/baekjun/Ex15652_2.java
@@ -0,0 +1,41 @@
+package baekjun;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Ex15652_2 {
+	private static int n, m;
+	private static int[] arr;
+	private static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		arr = new int[m];
+		
+		comb(0, 0);
+		System.out.println(sb);
+	}
+	
+	public static void comb(int start, int depth) {
+		if (depth == m) {
+			for (int val : arr) {
+				sb.append(val).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for (int i = start; i < n; i++) {
+			arr[depth] = i+1;
+			comb(i, depth+1);
+		}
+	}
+
+}
