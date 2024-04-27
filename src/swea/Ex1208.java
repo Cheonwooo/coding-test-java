d73833c96b34615902a5e09766e5b1bb032ed592
Chenowoo <cjsgusdn14@naver.com>
2024-04-27 13:51:52 +0900

    구현

---
+++ src/swea/Ex1208.java
@@ -0,0 +1,47 @@
+package swea;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Ex1208 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		for (int t = 1; t <= 10; t++) {
+			sb.append("#").append(t).append(" ");
+			int[] dumps = new int[100];
+			
+			int dumpCount = Integer.parseInt(br.readLine());
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < 100; i++) {
+				dumps[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			int answer = 0;
+			while (dumpCount != 0) {
+				dumpCount--;
+				
+				Arrays.sort(dumps);
+				
+				if (dumps[99] - dumps[0] <= 1) {
+					break;
+				}
+				
+				dumps[0]++;
+				dumps[99]--;
+			}
+			
+			Arrays.sort(dumps);
+			
+			answer = dumps[99] - dumps[0];
+			sb.append(answer).append("\n");
+		}
+		System.out.println(sb);
+	}
+
+}
