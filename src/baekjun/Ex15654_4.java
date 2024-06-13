0f39672a39e7d399a588e67188c9c7d2ef852c68
Chenowoo <cjsgusdn14@naver.com>
2024-06-13 18:59:29 +0900

    dfs

---
+++ src/baekjun/Ex15654_4.java
@@ -0,0 +1,53 @@
+package baekjun;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Ex15654_4 {
+	private static int n, m;
+	private static int[] arr, temp;
+	private static boolean[] visited;
+	private static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		arr = new int[n];
+		temp = new int[m];
+		visited = new boolean[n];
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		comb(0);
+		System.out.println(sb);
+	}
+	public static void comb(int depth) {
+		if (depth == m) {
+			for (int val : temp) {
+				sb.append(val + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for (int i = 0; i < n; i++) {
+			if (!visited[i]) {
+				visited[i] = true;
+				temp[depth] = arr[i];
+				comb(depth+1);
+				visited[i] = false;
+			}
+		}
+	}
+}
