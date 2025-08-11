be2c0ae295ced9b0ef42ca3e86ec663e431cefa9
cheonwooo <cjsgusdn14@naver.com>
2025-08-11 11:30:55 +0900

    dp

---
+++ src/baekjun/Ex15989.java
@@ -0,0 +1,34 @@
+package baekjun;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex15989 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine());
+		int[][] dp = new int[10001][4];
+		
+		dp[1][1] = 1;
+		dp[2][1] = 1;
+		dp[2][2] = 1;
+		dp[3][1] = 1;
+		dp[3][2] = 1;
+		dp[3][3] = 1;
+		
+		for (int i = 4; i < 10001; i++) {
+			dp[i][1] = dp[i-1][1];
+			dp[i][2] = dp[i-2][1] + dp[i-2][2];
+			dp[i][3] = dp[i-3][1] + dp[i-3][2] + dp[i-3][3];
+		}
+		
+		for (int t = 0; t < T; t++) {
+			int n = Integer.parseInt(br.readLine());
+			System.out.println(dp[n][1] + dp[n][2] + dp[n][3]);
+		}
+	}
+
+}
