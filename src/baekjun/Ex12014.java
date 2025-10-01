9c422816b3dc1c8f1221966ac56d23b05e77030b
cheonwooo <cjsgusdn14@naver.com>
2025-10-01 12:26:07 +0900

    LIS

---
+++ src/baekjun/Ex12014.java
@@ -0,0 +1,64 @@
+package baekjun;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Ex12014 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int T = Integer.parseInt(br.readLine());
+		for (int t = 0; t < T; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			int n = Integer.parseInt(st.nextToken());
+			int k = Integer.parseInt(st.nextToken());
+			int[] arr = new int[n];
+			int[] LIS = new int[n];
+			
+			st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < n; i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			LIS[0] = arr[0];
+			int len = 1;
+			
+			for (int i = 1; i < n; i++) {
+				int key = arr[i];
+				
+				if (LIS[len - 1] < key) {
+					len++;
+					LIS[len - 1] = key;
+				} else {
+					int left = 0;
+					int right = len;
+					
+					while (left < right) {
+						int mid = (left + right) / 2;
+						
+						if (LIS[mid] < key) {
+							left = mid + 1;
+						} else {
+							right = mid;
+						}
+					}
+					LIS[right] = key;
+				}
+			}
+			
+			sb.append("Case #" + (t+1) + "\n");
+			if (len >= k) {
+				sb.append(1 + "\n");
+			} else {
+				sb.append(0 + "\n");
+			}
+		}
+		System.out.println(sb);
+	}
+
+}
