a67f9d12d0b80e889394e92a5939d27c20294d77
cheonwooo <cjsgusdn14@naver.com>
2025-10-15 17:05:10 +0900

    implementation

---
+++ src/baekjun/Ex32775.java
@@ -0,0 +1,22 @@
+package baekjun;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex32775 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int s = Integer.parseInt(br.readLine());
+		int f = Integer.parseInt(br.readLine());
+		
+		if (s > f) {
+			System.out.println("flight");
+		} else {
+			System.out.println("high speed rail");
+		}
+	}
+
+}
