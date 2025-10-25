db3d237acfa1b065941a5451e03f2f0b3a088fbc
cheonwooo <cjsgusdn14@naver.com>
2025-10-26 00:00:06 +0900

    implementation

---
+++ src/baekjun/Ex25314.java
@@ -0,0 +1,24 @@
+package baekjun;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex25314 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n= Integer.parseInt(br.readLine());
+		
+		String typeName = "long ";
+		String answer = "";
+		for (int i = 0; i < n/4; i++) {
+			answer += typeName;
+		}
+		answer += "int";
+		
+		System.out.println(answer);
+	}
+
+}
