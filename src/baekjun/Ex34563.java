d4f153590a731fc97af71637d9ec833be0bb768b
cheonwooo <cjsgusdn14@naver.com>
2025-10-30 13:51:14 +0900

    Math

---
+++ src/baekjun/Ex34563.java
@@ -0,0 +1,24 @@
+package baekjun;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Ex34563 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for (int t = 0; t < T; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken());
+			int m = Integer.parseInt(st.nextToken());
+			
+			System.out.println(n + m - 1);
+		}
+	}
+
+}
