e2328ed090f6ca8195c680319ebc448610d32073
cheonwooo <cjsgusdn14@naver.com>
2025-11-24 16:26:25 +0900

    implementation

---
+++ src/baekjun/Ex9372.java
@@ -0,0 +1,25 @@
+package baekjun;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Ex9372 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine());
+		for (int t = 0; t < T; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			int n = Integer.parseInt(st.nextToken());
+			int m = Integer.parseInt(st.nextToken());
+			
+			for (int i = 0; i < m; i++) {
+				st = new StringTokenizer(br.readLine());
+			}
+			System.out.println(n - 1);
+		}
+	}
+}
