13d07c97db414425ada6143f3a9b91b8355667f5
cheonwooo <cjsgusdn14@naver.com>
2025-11-25 23:50:02 +0900

    implementation

---
+++ src/baekjun/Ex4101.java
@@ -0,0 +1,31 @@
+package baekjun;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Ex4101 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		while (true) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			if (a == 0 && b == 0) break;
+			
+			if (a <= b) {
+				sb.append("No" + "\n");
+			} else {
+				sb.append("Yes" + "\n");
+			}
+		}
+		System.out.println(sb);
+	}
+
+}
