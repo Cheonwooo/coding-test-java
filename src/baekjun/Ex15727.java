61557d167a405e2b807d685ebdd35e729683542c
cheonwooo <cjsgusdn14@naver.com>
2025-11-26 18:44:08 +0900

    implementation

---
+++ src/baekjun/Ex15727.java
@@ -0,0 +1,20 @@
+package baekjun;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex15727 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		if (n % 5 == 0) {
+			System.out.println(n / 5);
+		} else {
+			System.out.println(n / 5 + 1);
+		}
+	}
+
+}
