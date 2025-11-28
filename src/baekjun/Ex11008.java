9aff1de4a649219fff3d1336389a8652f26fe043
cheonwooo <cjsgusdn14@naver.com>
2025-11-28 11:45:19 +0900

    implementation

---
+++ src/baekjun/Ex11008.java
@@ -0,0 +1,27 @@
+package baekjun;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Ex11008 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for (int t = 0; t < T; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			String str = st.nextToken();
+			String word = st.nextToken();
+			
+			str = str.replaceAll(word, "*");
+			sb.append(str.length() + "\n");
+		}
+		System.out.println(sb);
+	}
+}
