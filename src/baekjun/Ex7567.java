72b7a1dc91d4d0f9ed62a1f06a7223e7c5ee54f9
cheonwooo <cjsgusdn14@naver.com>
2025-12-21 23:13:47 +0900

    implementation

---
+++ src/baekjun/Ex7567.java
@@ -0,0 +1,25 @@
+package baekjun;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex7567 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String input = br.readLine();
+		
+		int answer = 10;
+		for (int i = 1; i < input.length(); i++) {
+			if (input.charAt(i) == input.charAt(i - 1)) {
+				answer += 5;
+			} else {
+				answer += 10;
+			}
+		}
+		System.out.println(answer);
+	}
+
+}
