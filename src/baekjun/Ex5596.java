313971b53d57ace666e20bd6754fcb93648494ce
cheonwooo <cjsgusdn14@naver.com>
2025-12-22 20:41:07 +0900

    implementation

---
+++ src/baekjun/Ex5596.java
@@ -0,0 +1,30 @@
+package baekjun;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Ex5596 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int sumS = 0;
+		for (int i = 0; i < 4; i++) {
+			sumS += Integer.parseInt(st.nextToken());
+		}
+		
+		st = new StringTokenizer(br.readLine());
+		
+		int sumT = 0;
+		for (int i = 0; i < 4; i++) {
+			sumT += Integer.parseInt(st.nextToken());
+		}
+		
+		System.out.println(Math.max(sumS, sumT));
+	}
+
+}
