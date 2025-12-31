cfa73d078293a9cecda82ad8d3774b778096efb1
cheonwooo <cjsgusdn14@naver.com>
2025-12-31 20:07:33 +0900

    implementation

---
+++ src/baekjun/Ex11283.java
@@ -0,0 +1,17 @@
+package baekjun;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex11283 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		char input = br.readLine().charAt(0);
+		
+		System.out.println((int)input - 44031);
+	}
+
+}
