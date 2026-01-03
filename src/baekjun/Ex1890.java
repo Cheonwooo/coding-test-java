e5cfcd3151c936535cb928cea7d3a2363ae180a6
cheonwooo <cjsgusdn14@naver.com>
2026-01-03 14:48:03 +0900

    Dp

---
+++ src/baekjun/Ex1890.java
@@ -0,0 +1,39 @@
+package baekjun;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Ex1890 {
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int[][] arr = new int[n][n];
+		for (int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < n; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		long[][] dp = new long[n][n];
+		dp[0][0] = 1;
+		
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				int len = arr[i][j];
+				
+				if (len == 0) break;
+				if (i + len < n) dp[i + len][j] += dp[i][j];
+				if (j + len < n) dp[i][j + len] += dp[i][j];
+			}
+		}
+		
+		System.out.println(dp[n-1][n-1]);
+		
+	}
+}
