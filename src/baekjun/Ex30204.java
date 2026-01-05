b383058e7ab2a060da678dedb5f5ccb880ad0640
cheonwooo <cjsgusdn14@naver.com>
2026-01-05 18:08:45 +0900

    implementation

---
+++ src/baekjun/Ex30204.java
@@ -0,0 +1,30 @@
+package baekjun;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Ex30204 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int x = Integer.parseInt(st.nextToken());
+		
+		int sum = 0;
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			sum += Integer.parseInt(st.nextToken());
+		}
+		if (sum % x == 0) {
+			System.out.println(1);
+		} else {
+			System.out.println(0);
+		}
+	}
+
+}
