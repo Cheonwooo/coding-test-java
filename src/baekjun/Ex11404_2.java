ae4855373afde2bcc88cc8ee496c38a62bca8a8f
cheonwooo <cjsgusdn14@naver.com>
2026-04-04 14:48:51 +0900

    Floyd Warshall

---
+++ src/baekjun/Ex11404_2.java
@@ -0,0 +1,49 @@
+package baekjun;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Ex11404_2 {
+	
+	private static final int INF = (int) 1e9;
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		int m = Integer.parseInt(br.readLine());
+		int[][] dist = new int[n][n];
+		for (int i = 0; i < n; i++) {
+			Arrays.fill(dist[i], INF);
+			dist[i][i] = 0;
+		}
+		
+		for (int i = 0; i < m; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken())-1;
+			int b = Integer.parseInt(st.nextToken())-1;
+			int c = Integer.parseInt(st.nextToken());
+			
+			dist[a][b] = Math.min(dist[a][b], c);
+		}
+		
+		for (int k = 0; k < n; k++) {
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+				}
+			}
+		}
+		
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				System.out.print((dist[i][j] == INF) ? 0 + " " : dist[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+
+}
