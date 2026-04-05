0c88364dd4c06a9f35d11417e8331aeb975a12c1
cheonwooo <cjsgusdn14@naver.com>
2026-04-05 09:50:44 +0900

    Implementation

---
+++ src/baekjun/Ex10039.java
@@ -0,0 +1,21 @@
+package baekjun;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex10039 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int sum = 0;
+		for (int i = 0; i < 5; i++) {
+			int score = Integer.parseInt(br.readLine());
+			if (score < 40) score = 40;
+			sum += score;
+		}
+		System.out.println(sum / 5);
+	}
+
+}
